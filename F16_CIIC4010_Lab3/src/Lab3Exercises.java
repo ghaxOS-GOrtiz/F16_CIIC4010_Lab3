32fff94606dd600355105be0d488c0da49ef9fa9
ghaxOS <ghaxOS@10.31.44.166>
2016-08-31 10:51:59 -0400

    Changed title to "Gabriel C. Ortiz"

--- F16_CIIC4010_Lab3/src/Lab3Exercises.java
+++ F16_CIIC4010_Lab3/src/Lab3Exercises.java
@@ -3,9 +3,9 @@ import javax.swing.JFrame;
 public class Lab3Exercises {
 	public static void main(String[] args) {
 		// Changed title from "Hello, world!" to "Gabriel C. Ortiz Lopez"
-		JFrame myFrame = new JFrame("Gabriel C. Ortiz Lopez");
+		JFrame myFrame = new JFrame("Gabriel C. Ortiz");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		// Changes were made. Setting up for a 1366x768 resolution screen.
+		// Changes were made. Setting up for a 1366x768 resolution screen. 
 		myFrame.setLocation(573, 284);
 		// Changes were made. Switched from (400, 100) to (200, 200)
 		myFrame.setSize(200, 200);
