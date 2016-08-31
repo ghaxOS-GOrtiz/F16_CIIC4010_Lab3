00f28dd6bc50a61afca630de3fd9fac5bc0a1621
ghaxOS <ghaxOS@10.31.44.166>
2016-08-31 10:36:52 -0400

    Title, Location, and Size were changed.

--- F16_CIIC4010_Lab3/src/Lab3Exercises.java
+++ F16_CIIC4010_Lab3/src/Lab3Exercises.java
@@ -2,10 +2,13 @@ import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Hello, world!");
-		//myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setLocation(200, 200);
-		myFrame.setSize(400, 100);
+		// Changed title from "Hello, world!" to "Gabriel C. Ortiz Lopez"
+		JFrame myFrame = new JFrame("Gabriel C. Ortiz Lopez");
+		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		// Changes were made. Setting up for a 1366x768 resolution screen.
+		myFrame.setLocation(573, 284);
+		// Changes were made. Switched from (400, 100) to (200, 200)
+		myFrame.setSize(200, 200);
 		myFrame.setVisible(true);
 	}
-}
+} 
